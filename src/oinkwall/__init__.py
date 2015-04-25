6d2a8f9b9533c79a8bbbd5d2b236c52ffd8b3ba0
Hans van Kranenburg <hans.van.kranenburg@mendix.com>
2015-04-25 19:46:11 +0200

    Also import ipv4, ipv6 in module...
    ...do they're usable to define interfaces

--- src/oinkwall/__init__.py
+++ src/oinkwall/__init__.py
@@ -1,1 +1,6 @@
-from firewall import IPTables, IPTablesRuleset, HostsAllow, HostsAllowRuleset
+from firewall import IPTables
+from firewall import IPTablesRuleset
+from firewall import HostsAllow
+from firewall import HostsAllowRuleset
+from firewall import ipv4
+from firewall import ipv6
