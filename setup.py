b781c1d5bb29df175bee6c2737ef557eb88e2b2e
Hans van Kranenburg <hans@knorrie.org>
2018-12-14 23:28:30 +0100

    Prepare to release v0.5

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ with open('README.pypi') as f:
 setup(
     name='oinkwall',
     packages=['oinkwall'],
-    version='0.4.2',
+    version='0.5',
     description='Python module for programmatically creating low level iptables rule sets.',
     long_description=long_description,
     author='Hans van Kranenburg',
     author_email='hans@knorrie.org',
     url='https://github.com/knorrie/python-oinkwall',
-    download_url='https://github.com/knorrie/python-oinkwall/tarball/v0.4.2',
+    download_url='https://github.com/knorrie/python-oinkwall/tarball/v0.5',
     keywords=['firewall', 'iptables', 'ipv6'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
